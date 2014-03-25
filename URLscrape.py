4630d4d021b8f8d5f82e77174da55c80ecb85455
niek33 <nwhdewin@gmail.com>
2014-03-25 14:52:22 +0100

    Lekker

--- URLscrape.py
+++ URLscrape.py
@@ -4,10 +4,14 @@ from bs4 import BeautifulSoup
 import json
 import requests
 
-r=requests.get('http://www.reddit.com')
+r=requests.get('http://www.kieskeurig.nl/mobiel/gsm/apple/iphone-4s-8gb-zwart/prijzen/1568176')
 pageHTML=r.text
 soup=BeautifulSoup("".join(pageHTML))
-sAll=soup.findAll("a")
+sAll=soup.findAll("td", { "class" : "prijs" })
 
-for href in sAll:
-    print(href)
+
+for price in sAll:
+        for child in price:
+            prijs = child.string
+            print(prijs[19:])
+    
