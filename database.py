4204cfcda18442290a5dc512a59af68e6513cf5d
Niek de Win <nwhdewin@gmail.com>
2014-04-01 13:47:51 +0200

    connectie met database

--- database.py
+++ database.py
@@ -1,8 +1,13 @@
 import pymysql
 conn = pymysql.connect(host='zario.nl', unix_socket='/tmp/mysql.sock', user='zario_bigdata', passwd='w90vs', db='zario_bigdata')
 cur = conn.cursor()
+
+#Ophalen van records
 cur.execute("SELECT * FROM popularity")
-for response in cur:
-    print(response)
+for r in cur.fetchall():
+    print(r)
+
+# Invoeren van records
+cur.execute("INSERT INTO popularity VALUES (NULL ,  'Philips 1',  'tv',  'bol',  '1');");
 cur.close()
 conn.close()
